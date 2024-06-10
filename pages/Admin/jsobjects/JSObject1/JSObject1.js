3bec68f66d141493803139abe68b3814fd3b4865
Ajay Kumar D <ajay.kumar@innovaturelabs.com>
2024-06-10 06:22:56 +0000

    feat: added poll features

--- pages/Admin/jsobjects/JSObject1/JSObject1.js
+++ pages/Admin/jsobjects/JSObject1/JSObject1.js
@@ -18,35 +18,67 @@ export default {
 
 
 	},
-	
-		List2onItemClick: (value)=> {
+	PollSubmit:async()=>{
+		console.log(Poll_Form.data.poll)
+		try{
+			if(JSON.parse(Poll_Form.data.poll)){
+				
+       await insert_poll.run()
+				closeModal(("Modal3"))
+				  await showAlert('Poll inserted successfully', 'success');
+				await getpolls.run()
+		
+			}
+			else{
+
+				  await showAlert('Failed to insert poll', 'warning');
+								closeModal(("Modal3"))
+				
+			}
+
+		}catch(error){
+				  await showAlert('please check poll format', 'warning');
+			
+							closeModal(("Modal3"))
+
+			
+
+		}
+
+
+
+
+
+
+	},
+	List2onItemClick: (value)=> {
 		console.log(value)
-  
-		},
+
+	},
 	search: () => {
 		// if(selectAgent.selectedOptionValue.length==0){
-			// return select_work_orders.data
+		// return select_work_orders.data
 		// }
 		// else{
-			// return(select_work_orders.data.filter(user => user.agent==(selectAgent.selectedOptionLabel)))
+		// return(select_work_orders.data.filter(user => user.agent==(selectAgent.selectedOptionLabel)))
 		// }
 	},
 	getData: ()=>{
 		storeValue('tabledata',Query1.data)
 		console.log(appsmith.store.tabledata)
 	}
-,
+	,
 
 	totalHoursCalc: async() => {
 		let sum = 0
 		// if(selectAgent.selectedOptionValue.length == 0)
-			// sum = select_work_ordersPage.data.reduce((accumulator, object) => {
-				// return accumulator + object.hours;
-			// }, 0);
+		// sum = select_work_ordersPage.data.reduce((accumulator, object) => {
+		// return accumulator + object.hours;
+		// }, 0);
 		// else 
-			// sum = select_work_ordersAgentPage.data.reduce((accumulator, object) => {
-				// return accumulator + object.hours;
-			// }, 0);
+		// sum = select_work_ordersAgentPage.data.reduce((accumulator, object) => {
+		// return accumulator + object.hours;
+		// }, 0);
 		storeValue('hoursSum',sum);
 
 	},
@@ -57,9 +89,9 @@ export default {
 		let count = 0
 
 		// if(selectAgent.selectedOptionValue.length == 0)
-			// count = (select_work_ordersPage.data.filter((item) => item.completed == "Yes")).length;
+		// count = (select_work_ordersPage.data.filter((item) => item.completed == "Yes")).length;
 		// else 
-			// count = (select_work_ordersAgentPage.data.filter((item) => item.completed == "Yes")).length;
+		// count = (select_work_ordersAgentPage.data.filter((item) => item.completed == "Yes")).length;
 
 		storeValue('completedCount',count);
 	},
--- pages/graph/jsobjects/JSObject1/JSObject1.js
+++ pages/graph/jsobjects/JSObject1/JSObject1.js
@@ -1,7 +1,39 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	
+	PollSubmit:async()=>{
+		console.log(Poll_Form.data.poll)
+		try{
+			if(JSON.parse(Poll_Form.data.poll)){
+				
+       await insert_poll.run()
+				closeModal(("Modal3"))
+				  await showAlert('Poll inserted successfully', 'success');
+				await getpolls.run()
+		
+			}
+			else{
+
+				  await showAlert('Failed to insert poll', 'warning');
+								closeModal(("Modal3"))
+				
+			}
+
+		}catch(error){
+				  await showAlert('please check poll format', 'warning');
+			
+							closeModal(("Modal3"))
+
+			
+
+		}
+
+
+
+
+
+
+	},
 	clear: () => {
 		storeValue('hoursSum',null);
 		storeValue('completedCount',null);
